0777054729c42f7d3e420557e60360a76240cb31
SebaKrk <s.sciuba@gmail.com>
2024-11-16 20:17:28 +0100

    add GoogleService-Info and fireBase  package

---
+++ MyWorkout/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MyWorkout
+//
+//  Created by Sebastian Sciuba on 16/11/2024.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+    
+}
--- MyWorkout/MyWorkoutApp.swift
+++ MyWorkout/MyWorkoutApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct MyWorkoutApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    // MARK: - Lifecycle
+    init() {
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
+    }
+    
+    // MARK: - View
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
 }
