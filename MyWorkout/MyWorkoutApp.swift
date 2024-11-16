98fbbc5976cb7902b7c76871abb3511a1a2c4fc9
SebaKrk <s.sciuba@gmail.com>
2024-11-16 19:57:05 +0100

    Initial Commit

---
+++ MyWorkout/MyWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutApp.swift
+//  MyWorkout
+//
+//  Created by Sebastian Sciuba on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
