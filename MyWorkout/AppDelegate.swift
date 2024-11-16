4a847a7c2eec5951736d0e52487625a4d6cb2912
SebaKrk <s.sciuba@gmail.com>
2024-11-16 21:30:24 +0100

    add more FireBase package

--- MyWorkout/AppDelegate.swift
+++ MyWorkout/AppDelegate.swift
@@ -12,8 +12,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
+      FirebaseApp.configure()
+      return true
   }
     
 }
