89050707a55a0d0d151ebd9dd04cb45eb6059ec1
SebaKrk <s.sciuba@gmail.com>
2024-11-17 08:59:48 +0100

    IOS-0001 BasicPackage IOS-0001-A Container-Based Dependency Factory Package

---
+++ MyWorkout/Dependencies+ContainerExtension.swift
@@ -0,0 +1,13 @@
+//
+//  Dependencies+ContainerExtension.swift
+//  MyWorkout
+//
+//  Created by Sebastian Sciuba on 17/11/2024.
+//
+
+import Foundation
+import Factory
+
+extension Container {
+    
+}
