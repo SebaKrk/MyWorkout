d8389edfeff853e851f0bc07e7e6e1b2d2903718
SebaKrk <s.sciuba@gmail.com>
2024-11-17 10:19:59 +0100

    IOS-0001 BasicPackage IOS-0001-C AuthenticationService

--- MyWorkout/Authentication/AuthenticationService.swift
+++ MyWorkout/Authentication/AuthenticationService.swift
@@ -5,9 +5,14 @@
 //  Created by Sebastian Sciuba on 17/11/2024.
 //
 
-
 import Foundation
 
 protocol AuthenticationService {
+    
+    /// Method to authenticate the user with `email` and `password`.
+    func signIn(email: String, password: String) async throws
+    
+    /// Method for log out the user.
+    func signOut() async throws
     
 }
--- MyWorkout/Authentication/DefaultAuthenticationService.swift
+++ MyWorkout/Authentication/DefaultAuthenticationService.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 final class DefaultAuthenticationService: AuthenticationService {
+    
+    // MARK: - Properties
+    
+    private let auth: Auth
+    
+    // MARK: - Lifecycle
+    
+    init(auth: Auth =  .auth()) {
+        self.auth = auth
+    }
+    
+    // MARK: - API
+    
+    func signIn(email: String, password: String) async throws {
+        try await auth.signIn(withEmail: email, password: password)
+    }
+    
+    func signOut() async throws {
+        try auth.signOut()
+    }
     
 }
