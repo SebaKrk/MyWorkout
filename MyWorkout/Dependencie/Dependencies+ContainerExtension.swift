4e0b43880363be68ec2e52b3d668a0b6158b3bb6
Sebastian Ściuba <sebastian.sciuba@farmaprom.pl>
2024-11-22 12:58:39 +0100

    Dependencie

---
+++ MyWorkout/Dependencie/Dependencies+ContainerExtension.swift
@@ -0,0 +1,49 @@
+//
+//  Dependencies+ContainerExtension.swift
+//  MyWorkout
+//
+//  Created by Sebastian Sciuba on 17/11/2024.
+//
+
+import Foundation
+import Factory
+
+/// `Container` extension for managing dependencies.
+///
+/// This extension defines dependency factories, serving as a central point
+/// for managing instances in the application. It utilizes the [`Factory`](https://github.com/hmlongco/Factory) library,
+/// which simplifies dependency injection and improves the scalability of the codebase.
+///
+/// Example:
+/// ```swift
+/// // Access the shared instance of AuthenticationService
+/// let authenticationService = Container.shared.authenticationService()
+/// authenticationService.login(username: "test", password: "password")
+/// ```
+extension Container {
+    
+    /// Factory providing a shared instance of `AuthenticationService`.
+    ///
+    /// `authenticationService` is a shared component delivered by the dependency container.
+    /// By default, it returns an instance of `DefaultAuthenticationService`.
+    ///
+    /// - Properties:
+    ///   - `.shared`: The factory uses a shared instance, meaning that
+    ///     the same instance is reused throughout the application.
+    ///
+    /// - Returns: An object conforming to `AuthenticationService`.
+    var authenticationService: Factory<AuthenticationService> {
+        Factory(self) { DefaultAuthenticationService() }.shared
+    }
+    
+    /// Factory providing a shared instance of `AnyUserProvider`.
+    ///
+    /// `userProvider` is a shared component delivered by the dependency container.
+    /// By default, it returns an instance of `UserProvider`, which conforms to `AnyUserProvider`.
+    ///
+    var userProvider: Factory<AnyUserProvider> {
+        Factory(self) { UserProvider() }.shared
+    }
+    
+}
+
